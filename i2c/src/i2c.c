2ce36c5dbdfc2f597783ef09eed89b804010e339
AlejandroColomar <colomar.6.4.3@gmail.com>
2018-12-18 13:55:30 +0100

    Work in progress: i2c

--- i2c/src/i2c.c
+++ i2c/src/i2c.c
@@ -35,18 +35,21 @@
  ******* variables ************************************************************
  ******************************************************************************/
 /* Global --------------------------------------------------------------------*/
+	I2C_HandleTypeDef	i2c_handle;
+
 /* Static --------------------------------------------------------------------*/
 static	bool			init_pending	= true;
-static	I2C_HandleTypeDef	i2c_handle;
+
 /* Volatile ------------------------------------------------------------------*/
-static	volatile	bool	i2c_msg_pending;
 
 
 /******************************************************************************
  ******* static functions (declarations) **************************************
  ******************************************************************************/
+static	void	i2c_msp_init		(void);
 static	void	i2c_gpio_init		(void);
 static	void	i2c_nvic_conf		(void);
+
 static	int	i2c_peripherial_init	(void);
 static	int	i2c_filter_analog_conf	(void);
 static	int	i2c_filter_digital_conf	(void);
@@ -69,9 +72,8 @@ int	i2c_init	(void)
 		return	ERROR_OK;
 	}
 
-	__HAL_RCC_I2C1_CLK_ENABLE();
-	i2c_gpio_init();
-	i2c_nvic_conf();
+	i2c_msp_init();
+
 	if (i2c_peripherial_init()) {
 		error	|= ERROR_I2C_HAL_I2C_INIT;
 		error_handle();
@@ -92,92 +94,22 @@ int	i2c_init	(void)
 	return	ERROR_OK;
 }
 
-	/**
-	 * @brief	Transmit the message in data through I2C
-	 *		Sets global variable 'error'
-	 * @param	data:	data to transmit
-	 * @return	Error
-	 */
-int	can_msg_write	(uint8_t data [CAN_DATA_LEN])
-{
-	uint8_t	can_tx_data [CAN_DATA_LEN];
-	int	i;
-
-	if (init_pending) {
-		error	|= ERROR_CAN_INIT;
-		return	ERROR_NOK;
-	}
-
-	for (i = 0; i < CAN_DATA_LEN; i++) {
-		can_tx_data[i]	= data[i];
-	}
-
-	if (HAL_CAN_AddTxMessage(&can_handle, &can_tx_header, can_tx_data,
-							&can_tx_mailbox)) {
-		error	|= ERROR_CAN_HAL_ADD_TX_MSG;
-		error_handle();
-		return	ERROR_NOK;
-	}
-
-	return	ERROR_OK;
-}
-
-	/**
-	 * @brief	Return the data received
-	 *		Sets global variable 'error'
-	 * @param	data:	array where data is to be written
-	 * @return	Error
-	 */
-int	can_msg_read	(uint8_t data [CAN_DATA_LEN])
-{
-	int	i;
-
-	if (init_pending) {
-		error	|= ERROR_CAN_INIT;
-		return	ERROR_NOK;
-	}
-
-	if (!can_msg_pending) {
-		error	|= ERROR_CAN_NO_MSG;
-		return	ERROR_NOK;
-	}
-
-	for (i = 0; i < CAN_DATA_LEN; i++) {
-		data[i]	= can_rx_data[i];
-	}
-
-	can_msg_pending	= false;
-
-	return	ERROR_OK;
-}
-
 
 /******************************************************************************
  ******* HAL weak functions (redefinitions) ***********************************
  ******************************************************************************/
-/**
- * @brief	Rx Fifo 0 message pending callback
- * @param	hcan: pointer to a CAN_HandleTypeDef structure that contains
- *		the configuration information for the specified CAN.
- * @return	None
- */
-void	HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *can_handle_ptr)
-{
-	if (HAL_CAN_GetRxMessage(can_handle_ptr, CAN_RX_FIFO0, &can_rx_header,
-								can_rx_data)) {
-		error	|= ERROR_CAN_HAL_GET_RX_MSG;
-		error_handle();
-	} else if (can_msg_pending) {
-		error	|= ERROR_CAN_MSG_LOST;
-	} else {
-		can_msg_pending	= true;
-	}
-}
 
 
 /******************************************************************************
  ******* static functions (definitions) ***************************************
  ******************************************************************************/
+static	void	i2c_msp_init		(void)
+{
+	__HAL_RCC_I2C1_CLK_ENABLE();
+	i2c_gpio_init();
+	i2c_nvic_conf();
+}
+
 static	void	i2c_gpio_init		(void)
 {
 	GPIO_InitTypeDef	gpio_init_values;
